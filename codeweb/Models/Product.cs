f05ee6a3bf51ba982553495763265c1ee5f91691
Phan Xuân Thái <phanxuanthai2004@gmail.com>
2024-06-18 18:00:53 +0700

    Ném model lên (chưa dùng được đâu :^) )

---
+++ codeweb/Models/Product.cs
@@ -0,0 +1,46 @@
+namespace codeweb.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Web;
+
+    public partial class Product
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+            Price = 0;
+            ImagePro = "~/Image/Product/CuonTuiRac.jpg";
+        }
+
+        public int ProductID { get; set; }
+
+        public string NamePro { get; set; }
+
+        public int InvQuantity { get; set; }
+
+        public string DecriptionPro { get; set; }
+
+        [StringLength(20)]
+        public string IDCate { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public string ImagePro { get; set; }
+
+        [NotMapped]
+        public string OldImagePro { get; set; }
+
+        [NotMapped]
+        public HttpPostedFileBase UploadImage {  get; set; }
+
+        public virtual Category Category { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ codeweb/Scripts/IndexScript.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#slide1 .slide-container').slick({
+        lazyLoad: 'ondemand',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: "#slide1 .slick-prev",
+        nextArrow: "#slide1 .slick-next"
+    });
+
+    $('#slide2 .slide-container').slick({
+        lazyLoad: 'ondemand',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: "#slide2 .slick-prev",
+        nextArrow: "#slide2 .slick-next"
+    });
+
+});
