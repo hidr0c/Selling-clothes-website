60f21618dea6ff401e9c026aae091d1ce61269e0
FoxeiZ <foxeiz2004@gmail.com>
2024-06-19 10:23:00 +0700

    update dependencies add scafolding for some models resolve image loading

---
+++ codeweb/Controllers/BrandsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using codeweb.Models;
+
+namespace codeweb.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private DoAnEntities1 db = new DoAnEntities1();
+
+        // GET: Brands
+        public ActionResult Index()
+        {
+            return View(db.Brands.ToList());
+        }
+
+        // GET: Brands/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id.Trim());
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // GET: Brands/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Brands/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDBrand,NameBrand,Description")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Brands.Add(brand);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brand);
+        }
+
+        // GET: Brands/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brands/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDBrand,NameBrand,Description")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brand).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+
+        // GET: Brands/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Brand brand = db.Brands.Find(id);
+            db.Brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ codeweb/Controllers/ClothesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using codeweb.Models;
+
+namespace codeweb.Controllers
+{
+    public class ClothesController : Controller
+    {
+        private DoAnEntities1 db = new DoAnEntities1();
+
+        // GET: Clothes
+        public ActionResult Index()
+        {
+            var clothes = db.Clothes.Include(c => c.Brand).Include(c => c.Category);
+            return View(clothes.ToList());
+        }
+
+        // GET: Clothes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // GET: Clothes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDBrand = new SelectList(db.Brands, "IDBrand", "NameBrand");
+            ViewBag.IDCategory = new SelectList(db.Categories, "IDCategory", "NameCategory");
+            return View();
+        }
+
+        // POST: Clothes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDClothes,NameClothes,BuyPrice,SoldPrice,Discount,FinalSoldPrice,ClothesImage,Tax,IDBrand,IDCategory")] Cloth cloth)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clothes.Add(cloth);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDBrand = new SelectList(db.Brands, "IDBrand", "NameBrand", cloth.IDBrand);
+            ViewBag.IDCategory = new SelectList(db.Categories, "IDCategory", "NameCategory", cloth.IDCategory);
+            return View(cloth);
+        }
+
+        // GET: Clothes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDBrand = new SelectList(db.Brands, "IDBrand", "NameBrand", cloth.IDBrand);
+            ViewBag.IDCategory = new SelectList(db.Categories, "IDCategory", "NameCategory", cloth.IDCategory);
+            return View(cloth);
+        }
+
+        // POST: Clothes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDClothes,NameClothes,BuyPrice,SoldPrice,Discount,FinalSoldPrice,ClothesImage,Tax,IDBrand,IDCategory")] Cloth cloth)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cloth).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDBrand = new SelectList(db.Brands, "IDBrand", "NameBrand", cloth.IDBrand);
+            ViewBag.IDCategory = new SelectList(db.Categories, "IDCategory", "NameCategory", cloth.IDCategory);
+            return View(cloth);
+        }
+
+        // GET: Clothes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // POST: Clothes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Cloth cloth = db.Clothes.Find(id);
+            db.Clothes.Remove(cloth);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
