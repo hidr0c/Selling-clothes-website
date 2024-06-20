eefc488ba7d3248b4737dca9fc7d4fe2035808cf
Thảo Nguyên <22dh114652@st.huflit.edu.vn>
2024-06-21 05:50:38 +0700

    SignIn & LogIn View

---
+++ codeweb/Controllers/LogInController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codeweb.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
